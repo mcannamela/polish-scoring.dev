0a50e91694c3a3098e9b977e4f7af01e10f85728
Phillip Anderson <anderson.pa@gmail.com>
2013-04-21 21:02:56 +0100

    can now modify players

--- src/com/ultimatepolish/scorebookdb/Player.java
+++ src/com/ultimatepolish/scorebookdb/Player.java
@@ -187,7 +187,22 @@ public class Player{
 	public void setWeight_kg(int weight_kg) {
 		this.weight_kg = weight_kg;
 	}
+	
+	public boolean getRightHanded() {
+		return throwsRightHanded;
+	}
 
+	public void setRightHanded(boolean rightHanded) {
+		this.throwsRightHanded = rightHanded;
+	}
+
+	public boolean getLeftHanded() {
+		return throwsLeftHanded;
+	}
+
+	public void setLeftHanded(boolean leftHanded) {
+		this.throwsLeftHanded = leftHanded;
+	}
 
 	public int getnGames() {
 		return nGames;
