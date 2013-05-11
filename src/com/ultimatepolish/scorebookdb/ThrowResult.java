0a1cbeb91e10085fafcdc81feed444cb8489f4e7
Phillip Anderson <anderson.pa@gmail.com>
2013-05-11 01:18:37 +0100

    prep work for throws db changes

--- src/com/ultimatepolish/scorebookdb/ThrowResult.java
+++ src/com/ultimatepolish/scorebookdb/ThrowResult.java
@@ -4,5 +4,8 @@ public final class ThrowResult {
 	public static final int CATCH = 0;
 	public static final int DROP = 1;
 	public static final int STALWART = 2;
-	public static final int NA = 3;
+	public static final int BROKEN = 3;
+	public static final int NA = 4;
+	
+	public static final String[] typeString = {"Catch", "Drop", "Stalwart", "Broken", "NA"};
 }
