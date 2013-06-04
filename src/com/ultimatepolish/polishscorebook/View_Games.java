8866f0a056a58a68df77177b85274363e2caa428
mcannamela <mcannamela@localhost>
2013-06-03 21:35:15 -0400

    switched longpress/tap behavior for editing a GIP vs viewing game details

--- src/com/ultimatepolish/polishscorebook/View_Games.java
+++ src/com/ultimatepolish/polishscorebook/View_Games.java
@@ -160,7 +160,7 @@ public class View_Games extends MenuContainerActivity {
     	
    		// load the game in progress screen
         Long gid  = Long.valueOf(gameInfo.getId());
-		Intent intent = new Intent(getApplicationContext(), Detail_Game.class);
+		Intent intent = new Intent(getApplicationContext(), GameInProgress.class);
         intent.putExtra("GID", gid);
         startActivity(intent);
     	return true;
@@ -183,7 +183,7 @@ public class View_Games extends MenuContainerActivity {
             	
            		// load the game in progress screen
                 Long gid  = Long.valueOf(gameInfo.getId());
-        		Intent intent = new Intent(getApplicationContext(), GameInProgress.class);
+        		Intent intent = new Intent(getApplicationContext(), Detail_Game.class);
                 intent.putExtra("GID", gid);
                 startActivity(intent);
                 return true;
